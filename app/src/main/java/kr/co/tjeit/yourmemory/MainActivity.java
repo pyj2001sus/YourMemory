7626cb2acd015d6d4ee416bc14aecd4a27636d58
pyj2001sus <pyj2001sus@hanmail.net>
2017-11-27 14:55:29 +0900

    각 항목 프레그먼트 화면

--- app/src/main/java/kr/co/tjeit/yourmemory/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/yourmemory/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.co.tjeit.yourmemory;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity {
@@ -9,6 +11,10 @@ public class MainActivity extends BaseActivity {
     private ImageView newsfeedBtn;
     private ImageView noticeBtn;
     private ImageView seeMoreBtn;
+    private android.widget.FrameLayout homeFragment;
+    private android.widget.FrameLayout newsfeedFragment;
+    private android.widget.FrameLayout noticeFragment;
+    private android.widget.FrameLayout seeMoreFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,45 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.VISIBLE);
+                newsfeedFragment.setVisibility(View.GONE);
+                noticeFragment.setVisibility(View.GONE);
+                seeMoreFragment.setVisibility(View.GONE);
+            }
+        });
+
+        newsfeedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsfeedFragment.setVisibility(View.VISIBLE);
+                noticeFragment.setVisibility(View.GONE);
+                seeMoreFragment.setVisibility(View.GONE);
+            }
+        });
+
+        noticeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsfeedFragment.setVisibility(View.GONE);
+                noticeFragment.setVisibility(View.VISIBLE);
+                seeMoreFragment.setVisibility(View.GONE);
+            }
+        });
+
+        seeMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.setVisibility(View.GONE);
+                newsfeedFragment.setVisibility(View.GONE);
+                noticeFragment.setVisibility(View.GONE);
+                seeMoreFragment.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 
@@ -31,6 +76,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.seeMoreFragment = (FrameLayout) findViewById(R.id.seeMoreFragment);
+        this.noticeFragment = (FrameLayout) findViewById(R.id.noticeFragment);
+        this.newsfeedFragment = (FrameLayout) findViewById(R.id.newsfeedFragment);
+        this.homeFragment = (FrameLayout) findViewById(R.id.homeFragment);
         this.seeMoreBtn = (ImageView) findViewById(R.id.seeMoreBtn);
         this.noticeBtn = (ImageView) findViewById(R.id.noticeBtn);
         this.newsfeedBtn = (ImageView) findViewById(R.id.newsfeedBtn);
